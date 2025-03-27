9659d3804a9b99190fe3c0dd06854f474622e882
Aidan Anderson <AidanAnderson@users.noreply.github.com>
2025-03-27 14:57:53 -0400

    Update stub_lock.c

--- src/stub_lock.c
+++ src/stub_lock.c
@@ -4,15 +4,3 @@
 void __retarget_lock_init_recursive(void **lock) {
     (void)lock;
 }
-
-void __retarget_lock_acquire_recursive(void *lock) {
-    (void)lock;
-}
-
-void __retarget_lock_release_recursive(void *lock) {
-    (void)lock;
-}
-
-void __retarget_lock_close_recursive(void *lock) {
-    (void)lock;
-}
